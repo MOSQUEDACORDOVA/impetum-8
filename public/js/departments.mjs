ac7ad92c2a5eb9cb4611532db5e7e9bd63d1b9a7
Isaac Mosqueda <69749643+MOSQUEDACORDOVA@users.noreply.github.com>
2025-03-25 16:49:41 -0500

    datos reales

--- public/js/departments.mjs
+++ public/js/departments.mjs
@@ -3,9 +3,12 @@ export const departments = [
       id: 1,
       name: "Piombo 24",
       price:"Desde 3,100,000.00",
-      description:"Descripción corta",
+      description:`Invierte en Piombo 24 y asegura tu futuro con una propiedad de alto valor.`,
       details:"Detalles",
-      largeDescription:"Descripcion larga",
+      largeDescription:`
+        🏡 Preventa de departamentos con entrega programada para abril de 2026.✨ Solo 15 exclusivos departamentos de 2 recámaras y 2 baños completos, totalmente equipados.
+        <br><br> 📅 Agenda tu cita hoy mismo y sé parte de la experiencia Piombo 24.
+        <br><br>📩 Contáctanos para más información.`,
       image: "./assets/priombo1.jpg",
       rooms: 2,
       bathrooms: 2,
@@ -21,9 +24,18 @@ export const departments = [
       id: 2,
       name: "Tenis 55",
       price:"17,900,000",
-      description:"Descripción corta",
+      description:"🏡 Elige Tenis 55 y asegura tu inversión con un retorno óptimo.",
       details:"Detalles",
-      largeDescription:"Descripcion larga",
+      largeDescription:`
+      ✨ Exclusivos 7 departamentos de 2 recámaras y 2 baños completos, totalmente equipados.
+      <br><br>
+      🌿 Disfruta de un roof garden común y un diseño moderno pensado para tu comodidad.
+      <br><br>
+      🔹 Últimos departamentos disponibles.
+      <br><br>
+      📅 Agenda tu cita hoy y sé parte de la experiencia Tenis 55.
+      <br><br>
+      📩 Contáctanos para más información.`,
       image: "./assets/tenispreview.png",
       rooms: 2,
       bathrooms: 2,
@@ -37,10 +49,17 @@ export const departments = [
     },
     {
       id: 3,
-      name: "Azabache",
-      description:"Descripción corta",
+      name: "Azabache 86",
+      description:"Un Desarrollo Que Marcó Diferencia",
       details:"Detalles",
-      largeDescription:"Descripcion larga",
+      largeDescription:`
+      Azabache 86 fue un exclusivo desarrollo de departamentos nuevos, con precios desde $1,770,750.00 hasta $2,100,000.00, ubicado en Colonia La Estrella, Gustavo A. Madero, a pocos minutos del Metro Potrero y La Villa.
+      <br><br>
+      📍 Rodeado de escuelas, hospitales, mercados y centros comerciales, con fácil acceso a vías principales como Avenida Ferrocarril, Congreso de la Unión y Eje Vial.
+      <br><br>
+      🏡 Departamentos desde 40m² hasta 53m², con 1 recámara, 1 baño, cocina, sala, patio, roof garden y balcón.
+      <br><br>
+      Este desarrollo representó una excelente oportunidad de inversión y calidad de vida en una ubicación estratégica.`,
       image: "./assets/azabachepreview.jpg",
       rooms: "1+",
       bathrooms: "1+",
--- public/js/details.js
+++ public/js/details.js
@@ -16,34 +16,34 @@ const buildCard = (department)=>{
     const container = document.getElementById("details");
     const card = document.createElement("div");
     card.setAttribute("data-id", department.id);
-    card.classList.add("card-item" ,"w-[95%]", "md:w-[full]","p-2", "border" ,"border-black/32" ,"rounded-lg" ,"flex" ,"flex-col" ,"md:flex-row" ,"gap-4","md:h-[460px]");
+    card.classList.add("card-item" ,"w-[95%]", "md:w-[full]","p-2", "border" ,"border-black/32" ,"rounded-lg" ,"flex" ,"flex-col" ,"md:flex-row" ,"gap-4","md:h-[560px]", department.etiqueta);
     card.innerHTML = `
          <img
       class="w-full rounded-md object-cover md:w-[50%]"
       src="${department.image}"
       alt=""
       />
-      <div class="flex flex-col gap-2">
+      <div class="flex flex-col gap-4 justify-center">
       <p class="text-[28px]">${department.name}</p>
-      <p class ="text-xl text-[#D92741]">${department.price} MXN</p>
+      ${department.etiqueta !== "etiqueta_vendido" ? `<p class="text-xl text-[#D92741]">${department.price} MXN</p>` : ""}
       <p>${department.description}</p>
       <div class="flex gap-8">
-        <div class="flex gap-4">
+        <div class="flex gap-2 items-center">
         <img src="./assets/habitacion.svg" alt="" />
         <p>${department.rooms}</p>
         </div>
-        <div class="flex gap-4">
+        <div class="flex gap-2 items-center">
         <img src="./assets/bath.svg" alt="" />
         <p>${department.bathrooms}</p>
         </div>
-        <div class="flex gap-4">
+        <div class="flex gap-2 items-center">
         <img src="./assets/medidas.svg" alt="" />
         <p>${department.squareMeters}</p>
         </div>
       </div>
-      <div class="flex gap-3 my-6">
+      <div class="flex gap-3 my-4">
         <img
-        class="w-[23px] h-[23px]"
+        class="w-[20px] h-[20px]"
         src="./assets/localizacion.svg"
         alt="localizacion"
         />
@@ -52,11 +52,12 @@ const buildCard = (department)=>{
       <div
         class="flex flex-col gap-8 font-medium  border-black/32"
       >
-        <div
-          class="cursor-pointer w-[90%] md:max-w-[290px] border border-black/32 text-xl font-bold p-2 text-center rounded-full"
+        ${department.etiqueta !== "etiqueta_vendido" ? `
+        <a href="https://api.whatsapp.com/send?phone=5215624033930&text=Hola%20amigos%20de%20Impetum,%20me%20gustaría%20más%20información%20acerca%20de%20${encodeURIComponent(department.name)}"
+          class="cursor-pointer md:w-[90%] md:max-w-[290px] border border-black/32 text-xl font-bold p-2 text-center rounded-full"
         >
           Asesoría
-        </div>
+        </a>` : ""}
          <div
           class="flex flex-col gap-2"
         >
